fbc884ba0fd1e366b58a3fd949cf8d5347f40fc1
Kirill Romanov <kirban95@gmail.com>
2025-10-22 16:48:32 +0300

    feat: created base file

---
+++ task2/main.go
@@ -0,0 +1,38 @@
+package main
+
+type CircularQueue struct {
+	values []int
+	// need to implement
+}
+
+func NewCircularQueue(size int) CircularQueue {
+	return CircularQueue{} // need to implement
+}
+
+func (q *CircularQueue) Push(value int) bool {
+	return false // need to implement
+}
+
+func (q *CircularQueue) Pop() bool {
+	return false // need to implement
+}
+
+func (q *CircularQueue) Front() int {
+	return -1 // need to implement
+}
+
+func (q *CircularQueue) Back() int {
+	return -1 // need to implement
+}
+
+func (q *CircularQueue) Empty() bool {
+	return false // need to implement
+}
+
+func (q *CircularQueue) Full() bool {
+	return false // need to implement
+}
+
+func main() {
+
+}
