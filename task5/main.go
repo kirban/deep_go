3d9d7a9415a63e8372be89d613f4f69d347af18a
Kirill Romanov <kirban95@gmail.com>
2025-11-03 20:18:02 +0300

    feat: implemented Filter

--- task5/main.go
+++ task5/main.go
@@ -17,8 +17,15 @@ func Map(data []int, action func(int) int) []int {
 // Filter принимает функцию action и срез данных data.
 // Функция Filter должна вернуть новый срез, содержащий только те элементы data, для которых функция action возвращает true
 func Filter(data []int, action func(int) bool) []int {
-	// need to implement
-	return nil
+	result := make([]int, 0, cap(data))
+
+	for i := 0; i < len(data); i++ {
+		if action(data[i]) {
+			result = append(result, data[i])
+		}
+	}
+
+	return result
 }
 
 // Reduce принимает функцию action (функцию двух аргументов), срез данных data и начальное значение initial.
@@ -31,21 +38,21 @@ func Reduce(data []int, initial int, action func(int, int) int) int {
 func main() {
 	sl := []int{1, 2, 3, 4, 5, 6}
 
-	resultMap := Map(sl, func(el int) int {
-		fmt.Printf("el: %d\n", el)
-		return el * 2
-	})
-	fmt.Println(resultMap)
-
-	fmt.Println("---")
-
-	//resultFilter := Filter(sl, func(el int) bool {
-	//	return el%2 == 0
-	//
+	//resultMap := Map(sl, func(el int) int {
+	//	fmt.Printf("el: %d\n", el)
+	//	return el * 2
 	//})
-	//fmt.Printf("resultFilter: %d\n", resultFilter)
+	//fmt.Println(resultMap)
 	//
 	//fmt.Println("---")
+
+	resultFilter := Filter(sl, func(el int) bool {
+		return el%2 == 0
+
+	})
+	fmt.Printf("resultFilter: %d\n", resultFilter)
+
+	fmt.Println("---")
 	//
 	//reduceResult := Reduce(sl, 0, func(acc int, curr int) int {
 	//	return acc + curr
